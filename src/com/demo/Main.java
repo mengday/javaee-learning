3681bd52a51b821528fd0c0e7f5e137f691e8cf5
mengday <mengday@163.com>
2020-08-24 14:09:57 +0800

    init2

--- src/com/demo/Main.java
+++ src/com/demo/Main.java
@@ -3,7 +3,11 @@ package com.demo;
 public class Main {
 
     public static void main(String[] args) {
-        int num = 0;
-        System.out.println(1/num);
+        int result = div(10, 5);
+        System.out.println(result);
+    }
+
+    public static int div(int num, int num2) {
+        return num / num2;
     }
 }
