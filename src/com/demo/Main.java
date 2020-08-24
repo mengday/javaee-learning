ef38528b59c0d2d8edf803ecc88c79f944f46f20
mengday <mengday@163.com>
2020-08-24 14:06:18 +0800

    init2

--- src/com/demo/Main.java
+++ src/com/demo/Main.java
@@ -4,10 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         int num = 0;
-        if(num != 0) {
-            System.out.println(1/0);
-        } else {
-            System.out.println("num 不能为0");
-        } 
+        System.out.println(1/0);
     }
 }
