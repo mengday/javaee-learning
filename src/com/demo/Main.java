e83e765d3a4a4ec4fb78ad3908e9a140cc6913d3
mengday <mengday.zhang@gmail.com>
2020-08-24 14:02:28 +0800

    Update Main.java
    fixed bug

--- src/com/demo/Main.java
+++ src/com/demo/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         int num = 0;
-        System.out.println(1/0);
+        if(num != 0) {
+            System.out.println(1/0);
+        } else {
+            System.out.println("num 不能为0");
+        } 
     }
 }
